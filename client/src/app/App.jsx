722850ad7dc180b9cf936b02efe97db062e5a1e8
Adam <aorf@femmeosage.net>
2024-09-16 21:52:32 -0500

    got react router going

---
+++ client/src/app/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import MyBasements from './pages/MyBasements';
+// import Basement from '.pages/Basement';
+import CreateBasement from './pages/CreateBasement';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/my-basements" element={<MyBasements />} />
+      
+        <Route path="/create" element={<CreateBasement />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
