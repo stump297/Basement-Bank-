21d7e4362eac1beeded2738ca780db4741423609
Adam <aorf@femmeosage.net>
2024-09-16 22:16:25 -0500

    added the basement component

---
+++ client/src/app/pages/Basement.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Basement1() {
+  const [value, setValue] = useState(10000000);
+
+  return (
+    <div className="basement1">
+      <h1>Basement 1</h1>
+      <div>Current Value: ${value.toLocaleString()}</div>
+      <button onClick={() => setValue(value + 5)}>Increase by $5</button>
+      <button onClick={() => setValue(value - 5)}>Decrease by $5</button>
+    </div>
+  );
+}
+
+export default Basement1;
