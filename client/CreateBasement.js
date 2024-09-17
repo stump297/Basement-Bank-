b10a6035c0dd245b645a174e0ce23d5c12dccac2
Adam <aorf@femmeosage.net>
2024-09-16 21:01:01 -0500

    created the create basement component

---
+++ client/CreateBasement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CreateBasement() {
+  return (
+    <div className="create-basement">
+      <h1>Create a New Basement</h1>
+      <div>Step 1: ...</div>
+      <div>Step 2: ...</div>
+      <div>Step 3: ...</div>
+    </div>
+  );
+}
+
+export default CreateBasement;
