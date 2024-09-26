10ecc244bb7724a6fb6493396d91e3ee96fc59b4
Brian Moura <brian.prorealestate@gmail.com>
2024-09-25 22:53:32 -0400

    adding working graph component

---
+++ client/src/Graph.jsx
@@ -0,0 +1,30 @@
+// Working MVP Code
+import { useState } from 'react'
+
+import {
+    BarChart,
+    Bar,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+} from "recharts";
+ 
+// call values from volume and savings to populate the graph
+const App = () => {
+    // Sample data
+    const data = [
+        { name: "How much to fill basement", volume: 1400 },
+        { name: "Current Savings", savings: 700 },
+    ];
+ 
+    return (
+        <BarChart width={600} height={600} data={data}>
+            <Bar dataKey="students" fill="green" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+        </BarChart>
+    );
+};
+ 
+export default App;
